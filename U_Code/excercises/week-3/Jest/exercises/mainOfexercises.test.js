bba70b3610fef30d4037dbdbf8ff8eaaab4f46f0
Itay.Talias <itay.talias@gmail.com>
2022-09-05 17:15:08 +0300

    VSCode Pro

--- U_Code/excercises/week-3/Jest/exercises/mainOfexercises.test.js
+++ U_Code/excercises/week-3/Jest/exercises/mainOfexercises.test.js
@@ -16,16 +16,31 @@ test("isEven should return true if n is even, false otherwise", () => {
 //should remove at least one element from the array `arr`
 test("removeAtLeastOne should remove at least one element from the array `arr`", () => {
     const result = exercise.removeAtLeastOne([1, 2, 3, 4]);
-
     expect(result.length).toBeLessThan(4);
     expect(result).not.toContain(1);
+
+    const result2 = exercise.removeAtLeastOne([]);
+    expect(result2.length).toBe(0);
+
+    const result3 = exercise.removeAtLeastOne([1]);
+    expect(result3.length).toBeLessThan(1);
+    expect(result3).not.toContain(1);
 });
 
 //Exercise-3
 //should return a clean string without these symbols: "!", "#", ".", ",", "'"
 test("simplify should return a clean string without these symbols: ^^^^^^^^^", () => {
-    const result = exercise.simplify("hell'''o");
-    expect(result).toEqual("hello");
+    const result1 = exercise.simplify("hell'#!'o");
+    expect(result1).toEqual("hello");
+
+    const result2 = exercise.simplify("hello");
+    expect(result2).toEqual("hello");
+
+    const result3 = exercise.simplify(" ");
+    expect(result3).toEqual(" ");
+
+    const result4 = exercise.simplify("#!.. ");
+    expect(result4).toEqual(" ");
 });
 //Exercise-4
 test("It should verify that there is at least one boolean in the array", () => {
@@ -36,5 +51,5 @@ test("It should verify that there is at least one boolean in the array", () => {
     expect(result2).toBeTruthy();
 
     const result3 = exercise.validate([1, 2, 3, 4]);
-    expect(typeof result3).toEqual(typeof {});
+    expect(typeof result3).toEqual("object");
 });
---
+++ U_Code/excercises/week-4/example.js
@@ -0,0 +1,37 @@
+/*General function*/
+
+//fre-> forEach
+
+//fof-> forOf
+
+//fim->forIn
+
+//anfn->arrow function
+
+//nfn->new const function
+
+//sti->setInterval
+
+//sto->setTimeout
+
+//prom->promise
+
+//thenc->then+cach
+
+/*class helpers*/
+
+//con->c'tor
+
+//met->method
+
+//pge->get
+
+//pse->set
+
+/*console*/
+
+//cer->console.error
+
+//clg->console.log
+
+//file->preferences->UserSnnipets
