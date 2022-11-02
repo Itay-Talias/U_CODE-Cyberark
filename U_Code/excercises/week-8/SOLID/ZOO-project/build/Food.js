c3fe4256914297e4e19ce3d3576c5368df6e08d7
Itay.Talias <itay.talias@gmail.com>
2022-11-02 22:19:18 +0200

    create food class

---
+++ U_Code/excercises/week-8/SOLID/ZOO-project/build/Food.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Food {
+    constructor(type, dailyQuantity) {
+        this.type = type;
+        this.dailyQuantity = dailyQuantity;
+    }
+}
+exports.default = Food;
---
+++ U_Code/excercises/week-8/SOLID/ZOO-project/src/Food.ts
@@ -0,0 +1,10 @@
+class Food {
+    type: string;
+    dailyQuantity: number;
+    constructor(type: string, dailyQuantity: number) {
+        this.type = type;
+        this.dailyQuantity = dailyQuantity;
+    }
+}
+
+export default Food;
